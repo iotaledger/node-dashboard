787ad2467defe9ca842c142fa634eaf9c341ce18
Luca Moser <moser.luca@gmail.com>
2020-03-28 16:08:31 +0100

    Code inspection fixes and package renaming (#343)
    * fixes json key in peer.Info
    
    * refactor
    
    * fixes golinter complaints

---
+++ src/app/components/ExplorerTransactionQueryResult.tsx
@@ -0,0 +1,321 @@
+import * as React from 'react';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import {Tab, Nav} from "react-bootstrap";
+import NodeStore from "app/stores/NodeStore";
+import {inject, observer} from "mobx-react";
+import ExplorerStore from "app/stores/ExplorerStore";
+import Spinner from "react-bootstrap/Spinner";
+import ListGroup from "react-bootstrap/ListGroup";
+import Badge from "react-bootstrap/Badge";
+import * as dateformat from 'dateformat';
+import {Link} from 'react-router-dom';
+import {If} from 'tsx-control-statements/components';
+import ReactJson from 'react-json-view';
+import Alert from "react-bootstrap/Alert";
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClipboard, faClipboardCheck, faCode, faCheck } from '@fortawesome/free-solid-svg-icons';
+
+import * as style from '../../assets/main.css';
+
+interface Props {
+    nodeStore?: NodeStore;
+    explorerStore?: ExplorerStore;
+    match?: {
+        params: {
+            hash: string,
+        }
+    }
+}
+
+@inject("nodeStore")
+@inject("explorerStore")
+@observer
+export class ExplorerTransactionQueryResult extends React.Component<Props, any> {
+
+    componentDidMount() {
+        this.props.explorerStore.resetSearch();
+        this.props.explorerStore.searchTx(this.props.match.params.hash);
+    }
+
+    getSnapshotBeforeUpdate(prevProps: Props, prevState) {
+        if (prevProps.match.params.hash !== this.props.match.params.hash) {
+            this.props.explorerStore.searchTx(this.props.match.params.hash);
+        }
+        return null;
+    }
+
+    state = {
+        copied_hash: false,
+        copied_raw: false,
+    };
+
+    render() {
+        let {hash} = this.props.match.params;
+        let {tx, query_loading, query_err} = this.props.explorerStore;
+        let approversEle = [];
+        if (tx) {
+            if (tx.approvers) {
+                for (let i = 0; i < tx.approvers.length; i++) {
+                    let approversHash = tx.approvers[i];
+                    approversEle.push(
+                        <ListGroup.Item>
+                            <small>
+                                <Link to={`/explorer/tx/${approversHash}`}>{approversHash}</Link>
+                            </small>
+                        </ListGroup.Item>
+                    );
+                }
+            }
+        }
+        return (
+            <Container>
+            <If condition={query_err !== null}>
+                <Alert variant={"warning"}>
+                    Requested transaction unknown on this node!
+                </Alert>
+            </If>
+            <If condition={query_err === null}>
+                <h3>
+                    {
+                        tx ?
+                            <span>
+                                {
+                                    tx.is_milestone ?
+                                        <span>Milestone {tx.milestone_index}</span> :
+                                        'Transaction'
+                                }
+                            </span>
+                            :
+                            <span>Transaction</span>
+                    }
+                </h3>
+                <p>
+                    {hash} {' '}
+                    {
+                        tx &&
+                        <React.Fragment>
+                            <CopyToClipboard text={hash} onCopy={() => { 
+                                this.setState({copied_hash: true}); 
+                                const timer_hash = setTimeout(() => {
+                                    this.setState({copied_hash: false});
+                                }, 1000);
+                                return () => clearTimeout(timer_hash);
+                                }
+                            }>
+                                {this.state.copied_hash ? <FontAwesomeIcon icon={faClipboardCheck} /> : <FontAwesomeIcon icon={faClipboard} />}
+                            </CopyToClipboard>
+                            {' '}
+                            <CopyToClipboard text={tx.raw_trytes} onCopy={() => { 
+                                this.setState({copied_raw: true}); 
+                                const timer_raw = setTimeout(() => {
+                                    this.setState({copied_raw: false});
+                                }, 1000);
+                                return () => clearTimeout(timer_raw);
+                                }
+                            }>
+                                {this.state.copied_raw ? <FontAwesomeIcon icon={faCheck} /> : <FontAwesomeIcon icon={faCode} />}
+                            </CopyToClipboard>
+                            <br/>
+                            <span>
+                                <Badge variant="light">
+                                   Time: {dateformat(new Date(tx.timestamp * 1000), "dd.mm.yyyy HH:MM:ss")}
+                                    {
+                                        tx.attachment_timestamp !== 0 &&
+                                        <span>
+                                            {', '}Attachment Timestamp: {dateformat(new Date(tx.attachment_timestamp), "dd.mm.yyyy HH:MM:ss")}
+                                        </span>
+                                    }
+                                </Badge>
+                                {' '}
+                                {
+                                    tx.is_milestone ?
+                                        tx.confirmed.state ?
+                                            <Badge variant="success">
+                                                Confirmed
+                                            </Badge>
+                                            :
+                                            <Badge variant="primary">Valid</Badge>
+                                        :
+                                        tx.confirmed.state ?
+                                            <Badge variant="success">
+                                                Confirmed by Milestone {tx.confirmed.milestone_index}
+                                            </Badge>
+                                            :
+                                            <Badge variant="light">Unconfirmed</Badge>
+                                }
+                            </span>
+                        </React.Fragment>
+                    }
+                </p>
+                <Row className={"mb-3"}>
+                    <Col>
+                        {
+                            query_loading && <Spinner animation="border"/>
+                        }
+                    </Col>
+                </Row>
+                {
+                    tx &&
+                    <React.Fragment>
+                        <Row className={"mb-3"}>
+                            <Col>
+                                <ListGroup>
+                                    <ListGroup.Item>Value: {tx.value}i</ListGroup.Item>
+                                    <ListGroup.Item>
+                                        Tag: {' '}
+                                        <Link to={`/explorer/tag/${tx.tag}`}>
+                                            {tx.tag}
+                                        </Link>
+                                    </ListGroup.Item>
+                                    <ListGroup.Item>Obsolete Tag: {tx.obsolete_tag}</ListGroup.Item>
+                                </ListGroup>
+                            </Col>
+                            <Col>
+                                <ListGroup>
+                                    <ListGroup.Item>
+                                        Index: {' '}
+                                        {
+                                            tx.current_index !== 0 &&
+                                            tx.previous !== ''
+                                                ?
+                                                <Link className={style.prevNextButton}
+                                                      to={`/explorer/tx/${tx.previous}`}>
+                                                    {'< '}
+                                                </Link>
+                                                :
+                                                <Link
+                                                    className={[style.prevNextButton, style.hidden].join(" ")}
+                                                    to={`/explorer/tx/${tx.previous}`}
+                                                >
+                                                    {'< '}
+                                                </Link>
+                                        }
+                                        {tx.current_index}
+                                        /
+                                        {tx.last_index}
+                                        {
+                                            tx.current_index !== tx.last_index &&
+                                            tx.next !== ''
+                                            &&
+                                            <Link className={style.prevNextButton} to={`/explorer/tx/${tx.next}`}>
+                                                {' '} >
+                                            </Link>
+                                        }
+                                        {
+                                            tx.current_index === 0 &&
+                                            <React.Fragment>
+                                                {' '}
+                                                <Badge variant="light">Tail Transaction</Badge>
+                                            </React.Fragment>
+                                        }
+                                    </ListGroup.Item>
+                                    <ListGroup.Item>MWM: {tx.mwm}</ListGroup.Item>
+                                    <ListGroup.Item>Solid: {tx.solid ? 'Yes' : 'No'}</ListGroup.Item>
+                                </ListGroup>
+                            </Col>
+                        </Row>
+                        <Row className={"mb-3"}>
+                            <Col>
+                                <ListGroup>
+                                    <ListGroup.Item className="text-break">
+                                        Trunk: {' '}
+                                        <Link to={`/explorer/tx/${tx.trunk}`}>
+                                            {tx.trunk}
+                                        </Link>
+                                    </ListGroup.Item>
+                                </ListGroup>
+                            </Col>
+                            <Col>
+                                <ListGroup>
+                                    <ListGroup.Item className="text-break">
+                                        Branch: {' '}
+                                        <Link to={`/explorer/tx/${tx.branch}`}>
+                                            {tx.branch}
+                                        </Link>
+                                    </ListGroup.Item>
+                                </ListGroup>
+                            </Col>
+                        </Row>
+                        <Row className={"mb-3"}>
+                            <Col>
+                                <ListGroup>
+                                    <ListGroup.Item>
+                                        Address: {' '}
+                                        <Link to={`/explorer/addr/${tx.address}`}>
+                                            {tx.address}
+                                        </Link>
+                                    </ListGroup.Item>
+                                    <ListGroup.Item>
+                                        Bundle: {' '}
+                                        <Link to={`/explorer/bundle/${tx.bundle}`}>
+                                            {tx.bundle}
+                                        </Link>
+                                    </ListGroup.Item>
+                                    <ListGroup.Item>
+                                        Nonce: {tx.nonce}
+                                    </ListGroup.Item>
+                                    <ListGroup.Item className="text-break">
+                                        Message:<br/>
+                                        <Tab.Container id="left-tabs-message" defaultActiveKey="trytes">
+                                            <Row>
+                                                <Col sm={3}>
+                                                    <Nav variant="pills" className="flex-column">
+                                                        <Nav.Item>
+                                                            <Nav.Link eventKey="trytes">Trytes</Nav.Link>
+                                                        </Nav.Item>
+                                                        <Nav.Item>
+                                                            <Nav.Link eventKey="text">Text</Nav.Link>
+                                                        </Nav.Item>
+                                                        <If condition={tx.json_obj !== undefined}>
+                                                            <Nav.Item>
+                                                                <Nav.Link eventKey="json">JSON</Nav.Link>
+                                                            </Nav.Item>
+                                                        </If>
+                                                    </Nav>
+                                                </Col>
+                                                <Col sm={9}>
+                                                    <Tab.Content>
+                                                        <Tab.Pane eventKey="trytes">
+                                                            <small>
+                                                                {tx.signature_message_fragment}
+                                                            </small>
+                                                        </Tab.Pane>
+                                                        <Tab.Pane eventKey="text">
+                                                            <If condition={tx.ascii_message !== undefined}>
+                                                                {tx.ascii_message}
+                                                            </If>
+                                                        </Tab.Pane>
+                                                        <If condition={tx.json_obj !== undefined}>
+                                                            <Tab.Pane eventKey="json">
+                                                                    <ReactJson src={tx.json_obj} name={false}/>
+                                                            </Tab.Pane>
+                                                        </If>
+                                                    </Tab.Content>
+                                                </Col>
+                                            </Row>
+                                        </Tab.Container>
+                                    </ListGroup.Item>
+                                    <ListGroup.Item className="text-break">
+                                        Approvers: {' '}
+                                        <If condition={approversEle.length > 0}>
+                                            <ListGroup variant="flush">
+                                                {approversEle}
+                                            </ListGroup>
+                                        </If>
+                                        <If condition={approversEle.length === 0}>
+                                            No approvers yet
+                                        </If>
+                                    </ListGroup.Item>
+                                </ListGroup>
+                            </Col>
+                        </Row>
+                    </React.Fragment>
+                }
+            </If>
+            </Container>
+        );
+    }
+}
---
+++ src/app/components/Root.tsx
@@ -0,0 +1,116 @@
+import * as React from 'react';
+import {inject, observer} from "mobx-react";
+import NodeStore from "app/stores/NodeStore";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import {Dashboard} from "app/components/Dashboard";
+import Badge from "react-bootstrap/Badge";
+import {RouterStore} from 'mobx-react-router';
+import {Explorer} from "app/components/Explorer";
+import {NavExplorerSearchbar} from "app/components/NavExplorerSearchbar";
+import {Redirect, Route, Switch} from 'react-router-dom';
+import {LinkContainer} from 'react-router-bootstrap';
+import {ExplorerTransactionQueryResult} from "app/components/ExplorerTransactionQueryResult";
+import {ExplorerBundleQueryResult} from "app/components/ExplorerBundleQueryResult";
+import {ExplorerAddressQueryResult} from "app/components/ExplorerAddressResult";
+import {ExplorerTagQueryResult} from "app/components/ExplorerTagResult";
+import {Explorer404} from "app/components/Explorer404";
+import {Misc} from "app/components/Misc";
+import {Neighbors} from "app/components/Neighbors";
+import {Explorer420} from "app/components/Explorer420";
+import {Helmet} from 'react-helmet'
+
+interface Props {
+    history: any;
+    routerStore?: RouterStore;
+    nodeStore?: NodeStore;
+}
+
+@inject("nodeStore")
+@inject("routerStore")
+@observer
+export class Root extends React.Component<Props, any> {
+    renderDevTool() {
+        if (process.env.NODE_ENV !== 'production') {
+            const DevTools = require('mobx-react-devtools').default;
+            return <DevTools/>;
+        }
+    }
+
+    componentDidMount(): void {
+        this.props.nodeStore.connect();
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <Helmet>
+                    <title>{this.props.nodeStore.documentTitle}</title>
+                </Helmet>
+                <Navbar expand="lg" bg="light" variant="light" className={"mb-4"}>
+                    <Navbar.Brand>
+                        <img
+                            src="/assets/favicon.svg"
+                            width="40"
+                            className="d-inline-block"
+                            alt="Hornet"
+                        />
+                    </Navbar.Brand>
+                    <Nav className="mr-auto">
+                        <LinkContainer to="/dashboard">
+                            <Nav.Link>Dashboard</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/neighbors">
+                            <Nav.Link>Neighbors</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/explorer">
+                            <Nav.Link>
+                                Tangle Explorer
+                            </Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/debug">
+                            <Nav.Link>
+                                Misc
+                            </Nav.Link>
+                        </LinkContainer>
+                    </Nav>
+                    <a href="https://github.com/gohornet/hornet">
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                             className="navbar-brand"
+                             width="32"
+                             height="32"
+                             viewBox="0 0 16 16"
+                             focusable="false">
+                            <path fill="currentColor"
+                                  fill-rule="evenodd"
+                                  d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"/>
+                        </svg>
+                    </a>
+                    <Navbar.Collapse className="justify-content-end">
+                        <NavExplorerSearchbar/>
+                        <Navbar.Text>
+                            {!this.props.nodeStore.websocketConnected &&
+                            <Badge variant="danger">WS not connected!</Badge>
+                            }
+                        </Navbar.Text>
+                    </Navbar.Collapse>
+                </Navbar>
+                <Switch>
+                    <Route exact path="/dashboard" component={Dashboard}/>
+                    <Route exact path="/debug" component={Misc}/>
+                    <Route exact path="/neighbors" component={Neighbors}/>
+                    <Route exact path="/explorer/tx/:hash" component={ExplorerTransactionQueryResult}/>
+                    <Route exact path="/explorer/bundle/:hash" component={ExplorerBundleQueryResult}/>
+                    <Route exact path="/explorer/addr/:hash" component={ExplorerAddressQueryResult}/>
+                    <Route exact path="/explorer/tag/:hash" component={ExplorerTagQueryResult}/>
+                    <Route exact path="/explorer/404/:search" component={Explorer404}/>
+                    <Route exact path="/explorer/420" component={Explorer420}/>
+                    <Route exact path="/explorer" component={Explorer}/>
+                    <Redirect to="/dashboard"/>
+                </Switch>
+                {this.props.children}
+                {this.renderDevTool()}
+            </div>
+        );
+    }
+}
