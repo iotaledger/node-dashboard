97b73fd102a46ff53c8ee43f65a7a8a44cd23e01
Martyn Janes <martyn@obany.com>
2021-03-03 09:33:22 +0100

    Update to latest iota.js

--- src/app/components/tangle/ReceiptPayload.tsx
+++ src/app/components/tangle/ReceiptPayload.tsx
@@ -95,10 +95,10 @@ class ReceiptPayload extends Component<ReceiptPayloadProps, ReceiptPayloadState>
                     </div>
                 ))}
                 <div className="card--label">
-                    Input Transaction Milestone Hash
+                    Input Transaction Milestone Id
                 </div>
                 <div className="card--value card--value__mono">
-                    {this.props.payload.transaction.input.milestoneHash}
+                    {this.props.payload.transaction.input.milestoneId}
                 </div>
                 <div className="card--label">
                     Output Transaction Amount
