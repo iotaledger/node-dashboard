b9ca17e2b2877098c8b0cff2d79a29961f59080a
muXxer <mux3r@web.de>
2022-06-15 16:19:00 +0200

    Removed fields from IConfirmedMsMetrics

--- src/models/websocket/IConfirmedMsMetrics.ts
+++ src/models/websocket/IConfirmedMsMetrics.ts
@@ -1,8 +1,6 @@
 /* eslint-disable camelcase */
 export interface IConfirmedMsMetrics {
-    milestoneIndex: number;
     blocksPerSecond: number;
     referencedBlocksPerSecond: number;
     referencedRate: number;
-    timeSinceLastMilestone: number;
 }
